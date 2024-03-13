b529d7da10a6ae4d052b222f3353c2e73c84e19c
Justin Brubaker <justinbrubaker7@gmail.com>
2024-03-12 17:24:34 -0700

    Refactor UserProfile component layout and remove redundant code

--- src/components/home/UserProfile.tsx
+++ src/components/home/UserProfile.tsx
@@ -14,8 +14,20 @@ export default function UserProfile({ profile, key }: { profile: any; key: numbe
     return (
         <div
             key={key}
-            className='min-h-[350px] w-full mx-auto p-8 my-4 bg-white rounded-xl shadow-md grid grid-cols-2 gap-4 relative bg-gradient-to-t from-gray-100/50 via-gray-200/50 to-gray-100/50'
+            className='min-h-[350px] w-full  p-8 my-4 bg-white rounded-xl shadow-md  bg-gradient-to-t from-gray-100/50 via-gray-200/50 to-gray-100/50'
         >
+            <div className='text-center p-2 text-gray-primary'>
+                <h2 className='text-3xl font-bold text-gray-800'>{profile.name}</h2>
+            </div>
+            <div className='mb-4'>
+                <h4 className='text-xl font-bold text-gray-800 mt-4 '>About Me</h4>
+                <p>Gender: {profile.gender}</p>
+                <p>DOB: {profile.birth_year}</p>
+                <p>Height: {profile.height}</p>
+                <p>Weight: {profile.mass}</p>
+                <p>Hair Color: {profile.hair_color}</p>
+                <p>Eye Color: {profile.eye_color}</p>
+            </div>
             <div>
                 <div className='mb-4'>
                     <h4 className='text-xl font-bold text-gray-800'>Films</h4>
@@ -26,18 +38,6 @@ export default function UserProfile({ profile, key }: { profile: any; key: numbe
                         <h4 className='text-xl font-bold text-gray-800'>Starships Flown</h4>
                     ) : null}
                     {profile.starshipDetails ? renderStarships(profile.starshipDetails) : null}
-                </div>
-                <h4 className='text-xl font-bold text-gray-800 mt-4'>Details</h4>
-                <p>Hair Color: {profile.hair_color}</p>
-                <p>DOB: {profile.birth_year}</p>
-                <p>Eye Color: {profile.eye_color}</p>
-            </div>
-            <div className='absolute bottom-4 right-4 p-2 text-gray-primary'>
-                <h2 className='text-3xl font-bold text-gray-800'>{profile.name}</h2>
-                <div className='flex gap-x-2'>
-                    <p>H: {profile.height}</p>
-                    <p>W: {profile.mass}</p>
-                    <p>{profile.gender}</p>
                 </div>
             </div>
         </div>
